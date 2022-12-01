6311c0e184082c67f1577c077ee5db9e810001d3
rulhuda <nurulhuda.unhasy@gmail.com>
2022-12-01 15:43:23 +0700

    add secure cookie

--- src/controllers/Users.js
+++ src/controllers/Users.js
@@ -62,7 +62,7 @@ export const Login = async (req, res, next) => {
     res.cookie('refreshToken', refreshToken, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      // secure: true
+      secure: true
     });
 
     res.send({ error: false, accessToken })
